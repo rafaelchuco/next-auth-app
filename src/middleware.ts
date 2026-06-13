6c990b8af54e73120a34488ebf49dfc705e52dbd
rafaelchuco <rafael.chuco@tecsup.edu.pe>
2026-06-13 11:14:02 -0500

    fix: use nextauth built-in middleware to handle secure cookies in production

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,4 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
-
-export async function middleware(request: NextRequest) {
-  const token = await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-
-  if (!token) {
-    const signInUrl = new URL("/signIn", request.url);
-    signInUrl.searchParams.set("callbackUrl", request.nextUrl.pathname);
-    return NextResponse.redirect(signInUrl);
-  }
-
-  return NextResponse.next();
-}
+export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/dashboard/:path*", "/profile/:path*"],
